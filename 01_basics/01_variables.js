c0d326f1359e9429bddd14b5fcaee85ee07bd77d
prashantpatil5064-p3 <prashantpatil5064@gmail.com>
2026-06-10 11:21:45 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+/* Don't use var
+because  off isses in block and  functional scope 
+
+use only let 
+
+*/
+
+const accountId = 1235
+let accontEmail = "prashant@gmail.com"
+var accountPwd = "12345"
+accountCity = "Kolhapur"
+let accountState
+
+// accountId = 2  // not allowed 
+accontEmail = "pp@gmail.com"
+accountPwd = "3254456"
+accountCity = "kagal"
+
+console.log(accountId);
+console.table([accountId, accontEmail, accountPwd,accountCity])
+console.table([accountId, accontEmail, accountPwd,accountCity, accountState])
+// trtying git push 
