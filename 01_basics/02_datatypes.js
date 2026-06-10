5cab629a456cecd2408130a52c887827a6409b63
prashantpatil5064-p3 <prashantpatil5064@gmail.com>
2026-06-10 11:21:07 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; // trate all JS code as newer version
+
+//alert(5+5) // we are using node js, not browser
+
+console.log(3*6);
+
+
+//number = 2 to power of 53
+// bigint
+// string => ""
+//boolean==> true/false
+//null => standalon value
+//undefined =>
+// symbol => unique
+
+// object
+console.log(typeof undefined);
+console.log(typeof null);
+
